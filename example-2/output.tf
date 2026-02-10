4aac3794bca783bd99194018b2714f2cb197a319
akshayenuguru <akshay@rafay.co>
2026-02-10 10:32:24 +0530

    Update output.tf

--- example-2/output.tf
+++ example-2/output.tf
@@ -2,3 +2,8 @@ output "ec2_public_ip" {
   description = "Public IP of the EC2 instance"
   value       = aws_instance.example.public_ip
 }
+
+output "ec2_instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.example.id
+}
