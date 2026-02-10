eacb783ea7d5290077fb14b042e5687c788f9929
akshayenuguru <akshay@rafay.co>
2026-02-10 10:31:17 +0530

    Update main.tf

--- example-2/main.tf
+++ example-2/main.tf
@@ -1,3 +1,16 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
@@ -6,3 +19,4 @@ resource "aws_instance" "example" {
     Name = var.instance_name
   }
 }
+
